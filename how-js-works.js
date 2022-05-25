4d8ef4d1000a0d994a583abf9079b0b9dd7ff393
Daevah Taylaran <daevtayl@gmail.com>
2022-05-24 19:04:11 -0700

    more exercises

---
+++ how-js-works.js
@@ -0,0 +1,26 @@
+console.log('1');
+
+setTimeout(() => {
+  console.log('2');
+}, 2000);
+console.log('3');
+
+
+//CALL STACK
+callback()
+
+
+
+//WEB API
+
+
+
+//CALLBACK QUEUE
+
+
+
+//EVENT LOOP
+
+element.addEventListener('click', () => {
+  console.log('click');
+})
